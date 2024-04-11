1a78674697ea3b0012ddf4b8ebc7869734ea67d2
HectorLee1992 <102126232+HectorLee1992@users.noreply.github.com>
2024-04-11 23:34:59 +0800

    Update about.jsx
    Signed-off-by: HectorLee1992 <102126232+HectorLee1992@users.noreply.github.com>

--- src/containers/applications/apps/about.jsx
+++ src/containers/applications/apps/about.jsx
@@ -47,7 +47,7 @@ export const AboutWin = () => {
         <p className="pl-4">
           {t("about.contact")} :&nbsp;
           <a target="_blank" href="mailto:blue@win11react.com" rel="noreferrer">
-            blue@win11react.com
+            原作者信箱:blue@win11react.com
           </a>
         </p>
 
